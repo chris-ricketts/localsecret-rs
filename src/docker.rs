f68e2d41a05e58e9eb29b15b7b97e11df5af4f6c
Chris Ricketts <chris.ricketts@steribar.com>
2022-07-24 09:56:02 +0100

    feat: conditionally spawn a docker container or connect to an already running localsecret

--- src/docker.rs
+++ src/docker.rs
@@ -7,14 +7,14 @@ where
     let docker_args = [
         "-d",
         "-p",
-        &format!("{}:{}", consts::RPC_PORT, consts::RPC_PORT),
+        &format!("{}:{}", consts::DEFAULT_RPC_PORT, consts::DEFAULT_RPC_PORT),
         "-p",
         &format!("{}:{}", consts::FAUCET_PORT, consts::FAUCET_PORT),
         consts::DOCKER_IMAGE,
     ];
 
     cosmrs::dev::docker_run(docker_args, || {
-        let client = Client::init()?;
+        let client = Client::init(consts::DEFAULT_RPC_HOST, consts::DEFAULT_RPC_PORT)?;
         client.wait_for_first_block()?;
         f(&client)
     })
